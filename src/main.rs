6d6850b81c825cb88c979a453be7612e9acfd991
ddg <thewhiteless@yahoo.com>
2025-12-18 20:48:44 +0300

    Fermat-proving console app done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+fn fermat_proof(a: u32, b: u32, c: u32, n: u32) -> bool {
+    return !(a.pow(n) + b.pow(n) == c.pow(n) && n > 2);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_string = String::new();
+    println!("Enter values for a, b, c, and n (n > 2) separated by spaces:");
+    io::stdin().read_line(&mut input_string).expect("Failed to read line");
+
+    let inputs: Vec<u32> = input_string
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().expect("Please enter valid numbers"))
+        .collect();
+
+    let result = fermat_proof(inputs[0], inputs[1], inputs[2], inputs[3]);
+    if result {
+        println!("Fermat was right!");
+    } else {
+        println!("Fermat was wrong!");
+    }
 }
